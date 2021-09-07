005b149dfb67a98eefdb290601cdb1a3a0d8d9dd
WaysGry <84223582+WaysGry@users.noreply.github.com>
2021-09-07 10:19:05 +0500

    Add files via upload

---
+++ WpfApp8/WpfApp8/pages/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8.pages
+{
+    class Class1
+    {
+        public static Login PageLogin = new Login();
+        public static Registration PageReg = new Registration();
+        public static Page1 PagePag1 = new Page1();
+    }
+}
